f1238a768e49b1974460986e7f35861df060db0b
acoll <acoll@lle.rochester.edu>
2023-07-07 10:13:12 -0400

    Updated documentation, updated method call, added the resolution phase

--- src/static_analysis/__init__.py
+++ src/static_analysis/__init__.py
@@ -1,20 +1,25 @@
 from static_analysis.parsing import Parser
+from static_analysis.resolution import Resolver
 
 class StaticAnalyzer:
     
     def __init__(self, specifications):
         self.specifications = specifications
 
-    def analyze(self):
+    def run(self):
         """
         Finding the similar subprograms between each implementation, as well as the external variables and subprograms that are referenced by each.
-        :implementation_sources: Dictionary mapping the name of each implementation to the relative paths to its source files.
+        :implementations: List of objects representing each implementation.
         """
         
         # Parsing phase
         rootpaths = self.specifications.rootpaths
         filepaths = self.specifications.filepaths
         parser = Parser(rootpaths, filepaths)
-        implementations = parser.parse()
+        implementations = parser.run()
+        
+        # Resolution phase
+        resolver = Resolver(implementations)
+        implementations = resolver.run()
         
         return implementations
