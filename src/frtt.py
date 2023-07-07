0c1e3b9e8a7f3c68ace0f880662922fdcc77ef9c
acoll <acoll@lle.rochester.edu>
2023-07-07 11:52:59 -0400

    Updated print loop to output the results after resolution phase

--- src/frtt.py
+++ src/frtt.py
@@ -23,13 +23,16 @@ if __name__ == "__main__":
             for punit in src.programunits:
                 print("name : " + punit.name)
                 print("type : " + punit.type)
-                print("referenced_module_names: " + ("None" if not punit.referenced_module_names else ""))
-                for name in punit.referenced_module_names:
-                    print("\t" + name)
-                print("referenced_names: " + ("None" if not punit.referenced_names else ""))
-                for name in punit.referenced_names:
-                    print("\t" + name)
-                print("declared_names: " + ("None" if not punit.declared_names else ""))
-                for name in punit.declared_names:
-                    print("\t" + name)
+                print("referenced_modules: " + ("None" if not punit.referenced_modules else ""))
+                for module in punit.referenced_modules:
+                    print("\t" + module.name)
+                print("declared_variables: " + ("None" if not punit.declared_variables else ""))
+                for variable in punit.declared_variables:
+                    print("\t" + variable.name)
+                print("referenced_variables: " + ("None" if not punit.referenced_variables else ""))
+                for variable in punit.referenced_variables:
+                    print("\t" + variable.name)
+                print("referenced_programunits: " + ("None" if not punit.referenced_programunits else ""))
+                for programunit in punit.referenced_programunits:
+                    print("\t" + programunit.name)
                 print()
