9181e8ccc239751cc860ee02d8923ca6b13070df
acoll <acoll@lle.rochester.edu>
2023-07-13 10:27:55 -0400

    Experimenting with new way of representing a mapping

--- src/utilities/types/mapping.py
+++ src/utilities/types/mapping.py
@@ -1,29 +1,34 @@
-from collections import UserDict
+from __future__ import annotations
+from typing import TypeVar, Generic
 
-class Mapping:
+K = TypeVar('K')
+V = TypeVar('V')
+
+class Mapping(Generic[K, V]):
     
     def __init__(self):
-        self.map = dict()               # Dictionary that represents a mapping
-        self.domain = list()            # Domain of the mapping
+        self.map : dict[K, V] = dict()            # Dictionary that represents a mapping
+        self.domain : list[K] = list()            # Domain of the mapping
         
-    def __getitem__(self, key):
+    def __getitem__(self, key : K) -> V:
         return self.map[key]
     
-    def __setitem__(self, key, val):
+    def __setitem__(self, key : K, val : V):
         self.domain.append(key)
         self.map[key] = val
         
     def __iter__(self):
         return iter(self.domain)
     
-    def __contains__(self, key):
+    def __contains__(self, key : K) -> bool:
         return key in self.map
         
-    def get(self, key):
+    def get(self, key : K) -> V:
         return self.map.get(key)
     
-    def invert(self):
+    @classmethod
+    def invert(self) -> Mapping[V, K]:
         inverse = Mapping()
         for key in self:
             inverse[self[key]] = key
-            
+        return inverse
