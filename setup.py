82c019d9e6c9125f31b0b055b7ed54a44a89b490
acoll <acoll@lle.rochester.edu>
2023-09-23 13:08:33 -0400

    Created basic setup.py for use of frtt as package

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='frtt',
+      version='0.0',
+      description='Fortran Regression Testing Tool',
+      author='Aidan Collins',
+      author_email='aidancollinscs@gmail.com'
+     )
