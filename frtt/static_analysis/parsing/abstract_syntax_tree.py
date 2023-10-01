ec11df14b41156b9883914049411426dcf56d154
acoll <acoll@lle.rochester.edu>
2023-10-01 19:04:25 -0400

    Handled arbitrary strings as tokens in parse tree

--- frtt/static_analysis/parsing/abstract_syntax_tree.py
+++ frtt/static_analysis/parsing/abstract_syntax_tree.py
@@ -7,6 +7,7 @@ def get_abstract_syntax_tree(source_filepath):
     :source_filepath: Fortran source file to generate flang parse tree for
     :return: TreeNode representation of flang abstract syntax tree
     """
+    print("hello")
 
     # Get the raw string representation of AST
     raw_parse_tree = check_output("flang-new -fc1 -fdebug-dump-parse-tree-no-sema {}".format(source_filepath), shell=True, text=True)  
@@ -28,21 +29,70 @@ def get_abstract_syntax_tree(source_filepath):
     # Iterate over each line of output 
     for line in raw_parse_tree.split('\n'):
 
-        # Separate line into tokens
-        tokens = line.split(' ')
-        tokens = [i for i in tokens if i]
-        if not tokens: continue
+        if not line: continue
 
         # Extract the depth of the node indicated by line and its associated "->" separated keywords
         curr_depth = 0
+        curr_is_extended = False
+        keyword = ""
         keywords = []
-        for t in range(len(tokens)):
-            if tokens[t] == "|":
-                if not keywords:
-                    curr_depth += 1
-                    continue
-            if (tokens[t] != "->" and tokens[t] != "="):
-                keywords.append(tokens[t])
+        depth_state = 0
+        string_state = 1
+        gap_state = 2
+        keyword_state = 3
+        state = depth_state
+        for i in range(len(line)):
+            c = line[i]
+            match state:
+                case 0:                                         # Depth state
+                    match c:
+                        case " ":
+                            continue
+                        case "|":
+                            curr_depth += 1
+                        case "'":
+                            keyword = c
+                            state = string_state
+                        case default:
+                            keyword = c
+                            state = keyword_state
+                case 1:                                         # String state
+                    match c:
+                        case "'":
+                            keyword += c
+                            if i == len(line) - 1:
+                                keywords.append(keyword)
+                        case default:
+                            keyword += c
+                case 2:                                         # Gap state
+                    match c:
+                        case " " | "=":
+                            continue
+                        case "-" | ">":
+                            curr_is_extended = True
+                        case "'":
+                            keyword = c
+                            state = string_state
+                            curr_is_extended = False
+                        case default:
+                            keyword = c
+                            state = keyword_state
+                            curr_is_extended = False
+                case 3:                                         # Keyword state
+                    match c:
+                        case " ":
+                            keywords.append(keyword)
+                            state = gap_state
+                        case default:
+                            keyword += c
+                            if i == len(line) - 1:
+                                keywords.append(keyword)
+
+        # Add 1 to the depth of lines excluding the lines in the first program unit
+        if curr_depth == 0 and head:
+            parent = head
+            initial_program_unit = False
+        curr_depth += not initial_program_unit
 
         # Get the parent of the current node
         if curr_depth + prev_is_extended == prev_depth + 1:  # Node is at a depth of exactly one greater than the previous node (or depths are equal and previous line is extended)
@@ -77,7 +127,7 @@ def get_abstract_syntax_tree(source_filepath):
     
         # Update vars
         prev_node = curr_node
-        prev_is_extended = (tokens[-1] == "->")
+        prev_is_extended = curr_is_extended
         prev_depth = curr_depth
         line_num += 1
 
