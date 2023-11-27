c52c5d54864340d112b903212fdc6b038713c553
acoll <acoll@lle.rochester.edu>
2023-11-26 21:24:55 -0500

    Moved to new tree_analysis repo

--- exploratory/general/subclassable.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-from abc import ABC, abstractclassmethod
-from typing import Type
-
-class Subclassable(ABC):
-
-    @abstractclassmethod
-    def create_subclass(cls, clsname, *args, **kwargs) -> Type[Subclassable]:
-        pass
