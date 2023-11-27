ab1f6d3d49df1c6d72203e04dbc63a672aa409ae
acoll <acoll@lle.rochester.edu>
2023-11-26 21:25:17 -0500

    Fixed to filter out non-fortran source files

--- frtt/initial/initialize.py
+++ frtt/initial/initialize.py
@@ -1,6 +1,5 @@
 import os
-import fnmatch
-
+import re
 
 def initialize(rootpath : str):
     
@@ -12,12 +11,17 @@ def __get_filepaths(rootpath : str):
     
     # Initialize list to return
     filepaths = list()
+    print(rootpath)
     
     # Recursively search the rootpath directory
     for dirpath, _, fpaths in os.walk(rootpath):
         
-        # Iterate over each Fortran source filename
-        for fpath in fnmatch.filter(fpaths, "*.f*"):
+        # Iterate over each filename
+        for fpath in fpaths:
+            
+            # Continue if file extension is not fortran
+            extension = os.path.splitext(fpath)[1]
+            if re.search("\.[fF][0-9]*$", extension) is None: continue
             
             # Add the complete filepath to the list to return
             filepath = os.path.join(dirpath, fpath)
